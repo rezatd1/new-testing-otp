329575bb6460bee634d79fef74c4945497642c9c
Reza Torkashvand <reza.torkashvand.ins@gmail.com>
2023-10-14 17:15:53 +0330

    form delete

--- src/OTPForm.js
+++ src/OTPForm.js
@@ -5,17 +5,6 @@ function OTPForm() {
   const [otp, setOTP] = useState('');
   const inputRef = useRef(null);
 
-  const generateOTP = () => {
-    axios
-      .post('http://172.17.17.101:8088/api/en/Account/SignUp/SendVerificationCodeForResetPassword', {
-        countryCode: 98,
-        mobileWithOutCountryCode: '9390753192',
-      })
-      .catch((error) => {
-        console.error('Error sending OTP:', error);
-      });
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission if needed
@@ -24,12 +13,12 @@ function OTPForm() {
   useEffect(() => {
     if ('OTPCredential' in window) {
       const ac = new AbortController();
-      const form = inputRef.current?.closest('form');
-      if (form) {
-        form.addEventListener('submit', (e) => {
-          ac.abort();
-        });
-      }
+      // const form = inputRef.current?.closest('form');
+      // if (form) {
+      //   form.addEventListener('submit', (e) => {
+      //     ac.abort();
+      //   });
+      // }
       navigator.credentials
         .get({
           otp: { transport: ['sms'] },
@@ -37,7 +26,7 @@ function OTPForm() {
         })
         .then((otp) => {
           setOTP(otp.code);
-          if (form) form.submit();
+          // if (form) form.submit();
         })
         .catch((err) => {
           console.log(err);
